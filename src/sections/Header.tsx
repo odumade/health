73c9e473ced2dec4cf3c1272b912b5a92c3e443a
odumade <chekkitapp@gmail.com>
2024-09-12 15:06:50 +0100

    complete the banner and hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,12 +6,11 @@ import MenuIcon from '@/assets/menu.svg';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">Streamline your workflow and boot your productivity </p>
+    <header className="sticky top-0 backdrop-blur-sm">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-md gap-3">
+        <p className="text-white hidden md:block">Akroo Health, is committed to helping you achieve your mental health and behavioral goals through our virtual services </p>
         <div className="inline-flex gap-1 items-center">
-          <p>Get started for free</p>
-          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+          <p></p>
         </div>
       </div>
       <div className="py-5">
@@ -19,13 +18,12 @@ export const Header = () => {
           <div className="flex items-center justify-between">
             <Image src={Logo} alt='Sass Logo' height={40} width={40} />
             <MenuIcon className="h-5 w-5 md:hidden" />
-            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+            <nav className="hidden md:flex gap-6 text-black/60 items-center font-semibold">
               <a href="#">About</a>
-              <a href="#">Features</a>
-              <a href="#">Customers</a>
-              <a href="#">Update</a>
+              <a href="#">Practice Areas</a>
+              <a href="#">Faq</a>
               <a href="#">Help</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-item justify-center tracking-tight">Get For Free</button>
+              <button className="bg-black text-white  px-4 py-2 rounded-lg font-medium inline-flex align-item justify-center tracking-tight">Book an Appointment</button>
             </nav>
           </div>
         </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,46 @@
+import ArrowIcon from '@/assets/arrow-right.svg';
+import cogImage from '@/assets/remi.png';
+import Image from 'next/image';
+
 export const Hero = () => {
-  return null;
+  return (
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+      <div className="container mx-auto px-4">
+        <div className="md:flex md:items-center md:justify-between">
+          {/* Text Section */}
+          <div className="md:w-1/2">
+            {/* <div className="text-sm inline-flex border border-[#222]/10 px-3 p-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div> */}
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6 md:mt-12">
+            Meet Our Founder, DNP. Olurounke Remi Johnson
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+            Olurounke Remi Johnson (DNP, APN, FNP, PMHNP-BC) is dual board certified. She is qualified to 
+            diagnose and treat disorders in patients of all age groups. Some of her practice specialties 
+            include <span className="font-bold">Depression, Anxiety, Panic Disorders, Insomnia, ADHD, PTSD, Bipolar Disorder </span> and many more.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Book an Appointment</button>
+              {/* <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button> */}
+            </div>
+          </div>
+
+          {/* Image Section */}
+          <div className="mt-10 md:mt-0 md:w-1/2 md:flex md:justify-center">
+            <div className="relative md:h-[400px] lg:h-[500px] xl:h-[648px]">
+              <Image
+                src={cogImage}
+                alt="Cog Image"
+                className="h-full w-auto max-w-none md:absolute rounded-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
